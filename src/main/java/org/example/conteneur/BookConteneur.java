e089c443014f31054b9f77558ad47bcbfc8dc609
André Kouamé <kouameandre43@gmail.com>
2023-09-21 21:52:24 +0200

    saisie des livres

--- src/main/java/org/example/conteneur/BookConteneur.java
+++ src/main/java/org/example/conteneur/BookConteneur.java
@@ -2,22 +2,30 @@ package org.example.conteneur;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class BookConteneur {
     public static void main(String[] args){
-        Book book = new Book();
-        book.setId(1);
-        book.setTitle("Elle est belle la ville de Paris");
-
-        Book book2 = new Book();
-        book2.setId(2);
-        book2.setTitle("La tour eiffel oeuvre d'art");
-
+        char reponse = 'O';
         List<Book> books = new ArrayList<>();
-        books.add(book);
-        books.add(book2);
-        books.remove(book);
-
-        System.out.println(books.size());
+        int id = 1;
+        do{
+            Scanner scanner = new Scanner(System.in);
+            Scanner responseSaisie = new Scanner(System.in);
+            System.out.print( "Veuillez saisir le nom d'un livre : " );
+            String title = scanner.nextLine();
+            Book b = new Book();
+            b.setId(id);
+            b.setTitle(title);
+            books.add(b);
+            System.out.print( "Voulez vous continuez O(Oui), N(Non) " );
+            reponse = responseSaisie.nextLine().charAt(0);
+            System.out.println(reponse);
+            id++;
+        }while(reponse=='O');
+        for(Book b: books){
+            System.out.println(b.getId());
+            System.out.println(b.getTitle());
+        }
     }
 }
